c61c29f2ad58556a5651b0300c729bf9dcf8216d
sammouse <sammouse36@gmail.com>
2018-05-08 22:15:01 +0530

    adding pgm

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-alert('hello');
+console.log('Hello World');
+console.log(123);
+console.error('this is error');
+
+var firstName ="sam";
+console.log(firstName);
+var num = 123;
+var name;
+name="sam sundar";
+console.log(name);
+name="sam sundar c"
+console.log(name);
+//Let works in same way as const
+let secondName="sundar";
+console.log(secondName);
+
+//var and let works same way
+
+//
+
+const newName="sam";
+
+//with const you cant reassign variable to new value.
+//you need to assign some values,cant just declare like VAR and LET
